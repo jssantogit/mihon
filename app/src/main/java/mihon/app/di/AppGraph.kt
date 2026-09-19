0a0e7231eec87a36c71005d86b13cef94db5e07e
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-19 20:05:10 -0300

    Register Drive sync worker injection

--- app/src/main/java/mihon/app/di/AppGraph.kt
+++ app/src/main/java/mihon/app/di/AppGraph.kt
@@ -26,6 +26,7 @@ import eu.kanade.tachiyomi.data.library.LibraryUpdateJob
 import eu.kanade.tachiyomi.data.library.MetadataUpdateJob
 import eu.kanade.tachiyomi.data.notification.NotificationReceiver
 import eu.kanade.tachiyomi.data.track.TrackerManager
+import eu.kanade.tachiyomi.data.tsuzuki.drivesync.DriveSyncJob
 import eu.kanade.tachiyomi.data.tsuzuki.googleauth.GoogleAuthInteractiveCoordinator
 import eu.kanade.tachiyomi.data.updater.AppUpdateChecker
 import eu.kanade.tachiyomi.extension.ExtensionManager
@@ -64,6 +65,7 @@ interface AppGraph : ViewModelGraph {
     fun inject(webViewActivity: WebViewActivity)
     fun inject(baseOAuthLoginActivity: BaseOAuthLoginActivity)
     fun inject(libraryUpdateJob: LibraryUpdateJob)
+    fun inject(driveSyncJob: DriveSyncJob)
     fun inject(metadataUpdateJob: MetadataUpdateJob)
     fun inject(backupRestoreJob: BackupRestoreJob)
     fun inject(backupCreateJob: BackupCreateJob)
