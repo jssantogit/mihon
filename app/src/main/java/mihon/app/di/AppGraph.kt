ea2c82fa6a2e5ac15c10433ad574641de361cdc3
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-19 20:05:29 -0300

    Expose Drive sync runtime to settings

--- app/src/main/java/mihon/app/di/AppGraph.kt
+++ app/src/main/java/mihon/app/di/AppGraph.kt
@@ -27,6 +27,7 @@ import eu.kanade.tachiyomi.data.library.MetadataUpdateJob
 import eu.kanade.tachiyomi.data.notification.NotificationReceiver
 import eu.kanade.tachiyomi.data.track.TrackerManager
 import eu.kanade.tachiyomi.data.tsuzuki.drivesync.DriveSyncJob
+import eu.kanade.tachiyomi.data.tsuzuki.drivesync.DriveSyncRuntime
 import eu.kanade.tachiyomi.data.tsuzuki.googleauth.GoogleAuthInteractiveCoordinator
 import eu.kanade.tachiyomi.data.updater.AppUpdateChecker
 import eu.kanade.tachiyomi.extension.ExtensionManager
@@ -103,6 +104,7 @@ interface AppGraph : ViewModelGraph {
     val sourceManager: SourceManager
     val trackerManager: TrackerManager
     val googleAuthInteractiveCoordinator: GoogleAuthInteractiveCoordinator
+    val driveSyncRuntime: DriveSyncRuntime
     val extensionManager: ExtensionManager
     val chapterCache: ChapterCache
     val downloadCache: DownloadCache
