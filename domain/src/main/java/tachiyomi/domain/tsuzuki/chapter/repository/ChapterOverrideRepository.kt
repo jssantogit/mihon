98e6d65d7f5aa15b22cfc6cd196269052faf3e12
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-19 20:30:50 -0300

    Avoid reserved keyword in chapter override repository

--- domain/src/main/java/tachiyomi/domain/tsuzuki/chapter/repository/ChapterOverrideRepository.kt
+++ domain/src/main/java/tachiyomi/domain/tsuzuki/chapter/repository/ChapterOverrideRepository.kt
@@ -5,5 +5,5 @@ import tachiyomi.domain.tsuzuki.chapter.model.ChapterOverride
 interface ChapterOverrideRepository {
     suspend fun getById(id: String): ChapterOverride?
     suspend fun getAll(includeDeleted: Boolean = false): List<ChapterOverride>
-    suspend fun upsert(override: ChapterOverride)
+    suspend fun upsert(chapterOverride: ChapterOverride)
 }
