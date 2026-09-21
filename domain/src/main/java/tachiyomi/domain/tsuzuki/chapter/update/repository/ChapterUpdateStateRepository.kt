8fcd5a5ab94b3471f40243772a1a7d098c3b0ffb
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-21 14:54:27 -0300

    feat(tsuzuki): observe chapter update state

--- domain/src/main/java/tachiyomi/domain/tsuzuki/chapter/update/repository/ChapterUpdateStateRepository.kt
+++ domain/src/main/java/tachiyomi/domain/tsuzuki/chapter/update/repository/ChapterUpdateStateRepository.kt
@@ -1,11 +1,14 @@
 package tachiyomi.domain.tsuzuki.chapter.update.repository
 
+import kotlinx.coroutines.flow.Flow
 import tachiyomi.domain.tsuzuki.chapter.update.model.CanonicalChapterUpdateState
 
 interface ChapterUpdateStateRepository {
     suspend fun getAll(): List<CanonicalChapterUpdateState>
 
     suspend fun getByTitle(canonicalTitleId: String): List<CanonicalChapterUpdateState>
+
+    fun observeByTitle(canonicalTitleId: String): Flow<List<CanonicalChapterUpdateState>>
 
     suspend fun upsert(state: CanonicalChapterUpdateState)
 
