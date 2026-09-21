c7f287944bea0822044298fc90c2c11a9dad6046
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-21 05:35:47 -0300

    feat(tsuzuki): expose Mihon extensions as addons

---
+++ domain/src/main/java/tachiyomi/domain/tsuzuki/addon/repository/AddonRepository.kt
@@ -0,0 +1,13 @@
+package tachiyomi.domain.tsuzuki.addon.repository
+
+import kotlinx.coroutines.flow.Flow
+import tachiyomi.domain.tsuzuki.addon.AddonId
+import tachiyomi.domain.tsuzuki.addon.model.InstalledAddon
+
+interface AddonRepository {
+    fun observeInstalled(): Flow<List<InstalledAddon>>
+
+    suspend fun snapshot(): List<InstalledAddon>
+
+    suspend fun setEnabled(id: AddonId, enabled: Boolean)
+}
