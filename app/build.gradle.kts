22700ed6de2970a7d5198230ba03fd6a44cbc968
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-21 14:50:16 -0300

    refactor(tsuzuki): remove Google auth dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -311,9 +311,6 @@ dependencies {
     // RxJava
     implementation(libs.rxJava)
 
-    // Google account authorization
-    implementation(libs.google.playServicesAuth)
-
     // Networking
     implementation(libs.bundles.okhttp)
     implementation(libs.okio)
