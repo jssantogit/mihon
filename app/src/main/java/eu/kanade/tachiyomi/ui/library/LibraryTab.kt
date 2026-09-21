981e52cc41fa4e2ac4e79e7afb977e338787635a
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-21 19:48:05 -0300

    fix(tsuzuki): order Library as third tab

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryTab.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryTab.kt
@@ -40,7 +40,7 @@ data object LibraryTab : Tab {
             val isSelected = LocalTabNavigator.current.current.key == key
             val image = AnimatedImageVector.animatedVectorResource(R.drawable.anim_library_enter)
             return TabOptions(
-                index = 1u,
+                index = 2u,
                 title = stringResource(MR.strings.label_library),
                 icon = rememberAnimatedVectorPainter(image, isSelected),
             )
