86c88ff83add9362c0f6e87e6c4d80ea0962d191
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-21 15:45:01 -0300

    style(tsuzuki): sort reading sync test imports

--- domain/src/test/java/tachiyomi/domain/tsuzuki/sync/adapter/CanonicalReadingProgressSyncAdapterTest.kt
+++ domain/src/test/java/tachiyomi/domain/tsuzuki/sync/adapter/CanonicalReadingProgressSyncAdapterTest.kt
@@ -18,8 +18,8 @@ import tachiyomi.domain.tsuzuki.reader.repository.CanonicalReadingRepository
 import tachiyomi.domain.tsuzuki.sync.service.CanonicalTitleSyncSource
 import tachiyomi.domain.tsuzuki.sync.service.ChapterSyncEvidenceRepository
 import tachiyomi.domain.tsuzuki.sync.service.SyncClock
-import tachiyomi.domain.tsuzuki.sync.service.SyncRevisionSource
 import tachiyomi.domain.tsuzuki.sync.model.SyncRevision
+import tachiyomi.domain.tsuzuki.sync.service.SyncRevisionSource
 
 class CanonicalReadingProgressSyncAdapterTest {
 
