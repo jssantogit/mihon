a4a6305302fe86b6e0f3c0dfc00658626714b682
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-21 14:35:44 -0300

    style(tsuzuki): apply content ranking format [ci-full]

--- domain/src/main/java/tachiyomi/domain/tsuzuki/content/interactor/RankContentOptions.kt
+++ domain/src/main/java/tachiyomi/domain/tsuzuki/content/interactor/RankContentOptions.kt
@@ -35,7 +35,8 @@ class RankContentOptions {
 
     private fun deliveryRank(delivery: ContentDelivery): Int = when (delivery) {
         is ContentDelivery.LocalArchive,
-        is ContentDelivery.LocalDirectory -> 0
+        is ContentDelivery.LocalDirectory,
+        -> 0
         is ContentDelivery.Mihon -> 1
         is ContentDelivery.Torrent -> 2
     }
