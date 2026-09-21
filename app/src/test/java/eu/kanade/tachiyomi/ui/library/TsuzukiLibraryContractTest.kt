498781c66d27fd70e625f16faf0f94e5154841f8
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-21 19:42:40 -0300

    test(tsuzuki): specify canonical Library surface contract

---
+++ app/src/test/java/eu/kanade/tachiyomi/ui/library/TsuzukiLibraryContractTest.kt
@@ -0,0 +1,38 @@
+package eu.kanade.tachiyomi.ui.library
+
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import eu.kanade.tachiyomi.ui.tsuzuki.detail.CanonicalTitleScreen
+import eu.kanade.tachiyomi.ui.tsuzuki.library.CanonicalLibraryCardModel
+import eu.kanade.tachiyomi.ui.tsuzuki.library.canonicalLibraryDestination
+
+class TsuzukiLibraryContractTest {
+
+    @Test
+    fun `library card contract is canonical and source neutral`() {
+        val properties = CanonicalLibraryCardModel::class.java.declaredFields
+            .map { it.name }
+
+        properties shouldContainAll listOf(
+            "canonicalTitleId",
+            "title",
+            "status",
+            "categories",
+            "readingState",
+        )
+        properties shouldNotContain "identityState"
+        properties shouldNotContain "sourceId"
+        properties shouldNotContain "language"
+        properties shouldNotContain "sourceOrder"
+        properties shouldNotContain "readingSource"
+        properties shouldNotContain "sourceRepresentations"
+    }
+
+    @Test
+    fun `library item click opens canonical title detail`() {
+        canonicalLibraryDestination("canonical-1") shouldBe
+            CanonicalTitleScreen("canonical-1")
+    }
+}
