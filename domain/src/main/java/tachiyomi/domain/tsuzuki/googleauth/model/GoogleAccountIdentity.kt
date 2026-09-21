f5cf994c12fa00ce09437ec009559d42b7150ba1
João Santos <167802722+jssantogit@users.noreply.github.com>
2026-09-21 14:48:44 -0300

    refactor(tsuzuki): retire Google account runtime

--- domain/src/main/java/tachiyomi/domain/tsuzuki/googleauth/model/GoogleAccountIdentity.kt
+++
@@ -1,9 +0,0 @@
-package tachiyomi.domain.tsuzuki.googleauth.model
-
-data class GoogleAccountIdentity(
-    val accountName: String,
-) {
-    init {
-        require(accountName.isNotBlank()) { "Google account name must not be blank" }
-    }
-}
